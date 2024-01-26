6262633407a3970837044871242516b1bf9743eb
Roberto <robertorebolledo151@gmail.com>
2024-01-26 13:23:05 +0100

    to home

--- pythonProject2/Boom.py
+++ pythonProject2/Boom.py
@@ -10,23 +10,17 @@ class Boom(Consumable):
 
     def consume(self, player, scenery):
         super().consume(player, scenery)
-        left_corner = player.position[0]-self.radius,player.position[1]-self.radius,
+        left_corner = (player.position[0] - self.radius, player.position[1] - self.radius)
         area = []
         for a in range(self.radius):
-            coord = ((left_corner[0]+20)*a, (left_corner[1]+20)*a)
-            print(coord)
-            area.append(coord)
-            for element in scenery.obstacles:
-                if (coord[0] < element.position[0] + element.size[0] and
-                        coord[0] + coord[0] > element.position[0] and
-                        coord[1] < element.position[1] + element.size[1] and
-                        coord[1] + coord[1] > element.position[1]):
-                    #print(element.position[0],element.position[1])
-                    element.position[1] = -100
-                    element.position[0] = -100
-
-
-        print(player.position)
-        print(left_corner)
-        print(area)
+            for b in range(self.radius):
+                coord = (left_corner[0] + a * 20, left_corner[1] + b * 20)
+                area.append(coord)
+                for element in scenery.obstacles:
+                    if (coord[0] < element.position[0] + element.size[0] and
+                            coord[0] + 20 > element.position[0] and
+                            coord[1] < element.position[1] + element.size[1] and
+                            coord[1] + 20 > element.position[1]):
+                        if element.breakable:
+                            element.position = [-100, -100]
 
